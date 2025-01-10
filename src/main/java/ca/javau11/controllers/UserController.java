d3ed31acbc3ad71a87188808f47986a693a7c26d
pjblitz86 <paulius.juknis@gmail.com>
2025-01-10 08:08:12 +0200

    User controller and service

---
+++ src/main/java/ca/javau11/controllers/UserController.java
@@ -0,0 +1,24 @@
+package ca.javau11.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.javau11.entities.User;
+import ca.javau11.services.UserService;
+
+@RestController
+public class UserController {
+
+	private UserService userService;
+	
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping("user/add")
+	public User createUser(@RequestBody User user) {
+		return userService.addUser(user);
+	}
+	
+}
---
+++ src/main/java/ca/javau11/services/UserService.java
@@ -0,0 +1,21 @@
+package ca.javau11.services;
+
+import org.springframework.stereotype.Service;
+
+import ca.javau11.entities.User;
+import ca.javau11.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	private UserRepository userRepo;
+	
+	public UserService(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
+
+	public User addUser(User user) {
+		return userRepo.save(user);
+	}
+	
+}
