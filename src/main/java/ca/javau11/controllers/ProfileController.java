e4f1d83b0ffe2d2afeb7816b1aeaf424e0521921
pjblitz86 <paulius.juknis@gmail.com>
2025-01-10 08:10:23 +0200

    Profile controller and service

---
+++ src/main/java/ca/javau11/controllers/ProfileController.java
@@ -0,0 +1,57 @@
+package ca.javau11.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.javau11.entities.Profile;
+import ca.javau11.services.ProfileService;
+
+@RestController
+public class ProfileController {
+
+	private ProfileService profileService;
+	
+	public ProfileController(ProfileService profileService) {
+		this.profileService = profileService;
+	}
+	
+	@GetMapping("/profiles")
+	public List<Profile> getProfiles() {
+		return profileService.getProfiles();
+	}
+	
+	@GetMapping("/profile/{id}")
+	public ResponseEntity<Profile> getProfile(@PathVariable Long id) {
+		Optional<Profile> box = profileService.getProfile(id);
+		return ResponseEntity.of(box);
+	}
+	
+	@PostMapping("user/{id}/profile/add")
+	public Profile createProfile(@PathVariable Long id, @RequestBody Profile profile) {
+		return profileService.addProfile(id, profile);
+	}
+	
+	@PutMapping("profile/{id}")
+	public ResponseEntity<Profile> updateProfile(@PathVariable Long id, @RequestBody Profile profile) {
+		Optional<Profile> box = profileService.updateProfile(id, profile);
+		return ResponseEntity.of(box);
+	}
+	
+	@DeleteMapping("profile/{id}")
+	public ResponseEntity<Void> deleteProfile(@PathVariable Long id) {
+		boolean isDeleted = profileService.deleteProfile(id);
+		return isDeleted? 
+				ResponseEntity.ok().build() 
+				: ResponseEntity.notFound().build();
+	}
+	
+}
---
+++ src/main/java/ca/javau11/services/ProfileService.java
@@ -0,0 +1,69 @@
+package ca.javau11.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import ca.javau11.entities.Profile;
+import ca.javau11.entities.User;
+import ca.javau11.repositories.ProfileRepository;
+import ca.javau11.repositories.UserRepository;
+
+@Service
+public class ProfileService {
+
+	private UserRepository userRepo;
+	private ProfileRepository profileRepo;
+	
+	public ProfileService(UserRepository userRepo, ProfileRepository profileRepo) {
+		this.userRepo = userRepo;
+		this.profileRepo = profileRepo;
+	}
+	
+	public List<Profile> getProfiles() {
+		return profileRepo.findAll();
+	}
+	
+	public Optional<Profile> getProfile(Long id) {
+		return profileRepo.findById(id);
+	}
+	
+	public Profile addProfile(Long userId, Profile profile) {
+		
+        User user = userRepo.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        // TODO: add exception handling in separate files
+        
+        profile.setUser(user);
+        return profileRepo.save(profile);
+	}
+	
+	public Optional<Profile> updateProfile(Long id, Profile profile) {
+		Optional<Profile> box = profileRepo.findById(id);
+		if(box.isPresent()) {
+			Profile existingProfile = box.get(); // TODO: backend validation? and not sure about this method
+			existingProfile.setCompany(profile.getCompany());
+			existingProfile.setWebsite(profile.getWebsite());
+			existingProfile.setLocation(profile.getLocation());
+			existingProfile.setStatus(profile.getStatus());
+			existingProfile.setSkills(profile.getSkills());
+			existingProfile.setBio(profile.getBio());
+			existingProfile.setGithubUserName(profile.getGithubUserName());
+			return Optional.of(profileRepo.save(existingProfile));
+		}
+		
+		return Optional.empty();
+	}
+	
+	public boolean deleteProfile(Long id) {
+		Optional<Profile> box = profileRepo.findById(id);
+	    if (box.isEmpty()) return false;
+	    
+	    Profile profile = box.get();
+	    profile.setUser(null);
+	    
+	    profileRepo.delete(profile);
+	    return true;
+	}
+	
+}
